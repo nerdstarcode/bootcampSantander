cd0d03e6f3fff7818a0abdf72d4d2bc904cf56f7
nerdstarcode <sthivendev@gmail.com>
2022-07-06 16:56:01 -0300

    manipulando Dom

---
+++ DOM/assets/js/scripts.js
@@ -0,0 +1,31 @@
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+const darkModeClass = 'dark-mode';
+function changeMode(){
+    changeClasses();
+    changeText();
+}
+
+function changeClasses(){
+    button.classList.toggle(darkModeClass);
+    h1.classList.toggle(darkModeClass)
+    body.classList.toggle(darkModeClass);
+    footer.classList.toggle(darkModeClass);
+    return;
+}
+
+function changeText(){
+    const lightMode = "Light Mode";
+    const darkMode = 'Dark Mode';
+    if(body.classList.contains(darkModeClass)){
+        button.innerHTML = lightMode;
+        h1.innerHTML = darkMode + " On";
+        return;
+    }
+        button.innerHTML = darkMode;
+        h1.innerHTML = lightMode + " On";
+    return;
+}
+button.addEventListener('click', changeMode);
